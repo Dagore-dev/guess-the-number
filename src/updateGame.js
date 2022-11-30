54a1cde1071b8acd17999a488ddff808d4b0aa33
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-11-30 14:19:46 +0100

    replaced alert with custom modal

--- src/updateGame.js
+++ src/updateGame.js
@@ -1,13 +1,19 @@
+import { createModal } from './utils/createModal.js'
+
 /**
  * Check if the user hit the correct number and disable the invalid buttons
  * @param {number} random
  * @param {number} selectedNumber
  */
 export function updateGame (random, selectedNumber) {
+  const buttonsContainer = document.getElementById('buttons-container')
   const buttons = document.querySelectorAll('.grid button')
 
   if (random === selectedNumber) {
-    window.alert(`¡Correcto! El número secreto era ${random}`)
+    const dialog = createModal(`¡Correcto! El número secreto era ${random}`)
+    buttonsContainer.appendChild(dialog)
+    dialog.open = true
+
     buttons.forEach(btn => {
       btn.disabled = true
     })
